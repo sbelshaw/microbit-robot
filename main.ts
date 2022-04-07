3bd5b7b14c2950564d91037a2069250344d2ee76
sbelshaw <sbelshaw@users.noreply.github.com>
2022-04-07 18:23:49 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,14 +2,25 @@ enum RadioMessage {
     message1 = 49434,
     right = 32391,
     left = 14947,
-    startstop = 5388
+    startstop = 5388,
+    stopstart = 20043
 }
 input.onButtonPressed(Button.A, function () {
     radio.sendMessage(RadioMessage.right)
 })
 input.onButtonPressed(Button.AB, function () {
-    radio.sendMessage(RadioMessage.startstop)
+    radio.sendMessage(RadioMessage.stopstart)
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendMessage(RadioMessage.left)
+})
+radio.setGroup(3)
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
 })
